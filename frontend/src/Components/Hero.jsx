d5e51a27894e0932156d23d911dec5011be366a8
Janith-Nanayakkara <141998810+JanithNanayakkaraSE0612@users.noreply.github.com>
2024-08-11 17:37:54 +0530

    Update

--- frontend/src/Components/Hero.jsx
+++ frontend/src/Components/Hero.jsx
@@ -1,51 +1,53 @@
+import React from "react";
+import Heading from "./Heading"; // Adjust the path if necessary
 import HeroImg from "../assests/Home/home.jpg";
 
 const Hero = () => {
   return (
-    <div
-      className="relative z-0 flex-wrap min-h-screen gap-2 md:-mt-10 flex-center-center"
-      style={{
-        // background:  `url(${HeroImg})`,
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "contain",
-      }}
+    <section
+      className="hero bg-cover bg-center h-[90vh] w-full"
+      style={{ backgroundImage: `url(${HeroImg})` }}
     >
-      <div className="absolute top-0 right-0 rounded-full bg-[#04a7ff]/30 dark:bg-[#04a7ff]/50 w-72 h-72 -z-10 blur-[120px]"></div>
-      <div className="flex-1 basis-[20rem]">
-        <h1 className="text-4xl font-bold capitalize md:text-5xl">
-        Find Your New property Today <br /> With Us.
-        </h1>
-        <div className="pl-3 mt-5 border-l-4 border-primary">
-          <p>
-          Finding the home, commercial, or office you’ll love to rent or buy just got easier.
-          </p>
-        </div>
-        <button className="mt-6 btn btn-primary">get started</button>
-        <div className="mt-6 text-center flex-align-center gap-x-6">
-          <div>
-            <h1 className="text-2xl font-bold">
-              12k <span className="text-sm text-primary">+</span>
-            </h1>
-            <p>Requested Projects</p>
+      <div className="container pt-[15%]">
+        <Heading
+          title="Search Your Next Home"
+          subtitle="Find new & featured property located in your local city."
+        />
+
+        <form className="flex bg-white rounded-[5px] mt-[50px] px-[20px]">
+          <div className="box p-[15px] first:border-l-0 border-l border-gray-300">
+            <span className="text-sm text-gray-500">City/Street</span>
+            <input
+              type="text"
+              placeholder="Location"
+              className="p-[10px] w-full border border-gray-300 mt-[5px] rounded-[5px] placeholder-black text-lg"
+            />
           </div>
-          <div>
-            <h1 className="text-2xl font-bold">
-              15k <span className="text-sm text-primary">+</span>
-            </h1>
-            <p>Projects Completed</p>
+          <div className="box p-[15px] border-l border-gray-300">
+            <span className="text-sm text-gray-500">Property Type</span>
+            <input
+              type="text"
+              placeholder="Property Type"
+              className="p-[10px] w-full border border-gray-300 mt-[5px] rounded-[5px] placeholder-black text-lg"
+            />
           </div>
-          <div>
-            <h1 className="text-2xl font-bold">
-              100 <span className="text-sm text-primary">+</span>
-            </h1>
-            <p>Served Clients</p>
+          <div className="box p-[15px] border-l border-gray-300">
+            <span className="text-sm text-gray-500">Price Range</span>
+            <input
+              type="text"
+              placeholder="Price Range"
+              className="p-[10px] w-full border border-gray-300 mt-[5px] rounded-[5px] placeholder-black text-lg"
+            />
           </div>
-        </div>
+          <div className="box p-[15px] border-l border-gray-300">
+            <h4 className="font-medium">Advance Filter</h4>
+          </div>
+          <button className="btn1">
+            <i className="fa fa-search"></i>
+          </button>
+        </form>
       </div>
-      <div className="flex-1 basis-[20rem]">
-        <img src={HeroImg} alt="property image" className="w-full" />
-      </div>
-    </div>
+    </section>
   );
 };
 
