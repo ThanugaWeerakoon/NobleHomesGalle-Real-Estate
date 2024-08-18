5bbac2c1e5e386d6fb4f702a10226a9310659af0
Janith-Nanayakkara <141998810+JanithNanayakkaraSE0612@users.noreply.github.com>
2024-08-18 13:22:35 +0530

    Update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 
 import './App.css'
+import TopNav from './Components/TopNav'
 import { Home } from './Pages/Home/Home'
 
 function App() {
 
   return (
     <>
+    <TopNav/>
      <Home/>
     </>
   )
