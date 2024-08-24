9fa2d5f6e22b7451e88f0f0563b8d65d37487e4f
PasinduPabasaraPitigala <122001529+PasinduPabasaraPitigala@users.noreply.github.com>
2024-08-25 02:23:46 +0530

    commite

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,23 @@
 
-import './App.css'
-import TopNav from './Components/TopNav'
-import { Home } from './Pages/Home/Home'
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home/Home.jsx";
+import Contact from "./Pages/Contact/Contact.jsx";
+// import TopNav from '/Components/TopNav';
 
-function App() {
-
+export default function App() {
   return (
-    <>
-    <TopNav/>
-     <Home/>
-    </>
-  )
+    <BrowserRouter>
+     {/* <TopNav/> */}
+      <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/contact" element={<Contact />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
+
+
