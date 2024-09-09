4473aa8d96bacf3b07f14a7a2c69453d1c9af347
PasinduPabasaraPitigala <122001529+PasinduPabasaraPitigala@users.noreply.github.com>
2024-09-09 17:22:14 +0530

    commit

--- frontend/src/Components/TopNav.jsx
+++ frontend/src/Components/TopNav.jsx
@@ -82,7 +82,7 @@ const TopNav = () => {
         color="blue-gray"
         className="p-1 font-normal"
       >
-        <a href="#Contact" className="flex items-center">
+        <a href="contact" className="flex items-center">
           Contact
         </a>
       </Typography>
