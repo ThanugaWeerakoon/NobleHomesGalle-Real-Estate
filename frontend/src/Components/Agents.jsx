24db66850fd859240e4ec3da8adf3f9c052c5264
Janith-Nanayakkara <141998810+JanithNanayakkaraSE0612@users.noreply.github.com>
2024-10-30 20:42:56 +0530

    Update agent

--- frontend/src/Components/Agents.jsx
+++ frontend/src/Components/Agents.jsx
@@ -1,64 +1,53 @@
-import React from 'react'
+import React from 'react';
 import client1 from "../assests/images/client1.png";
 import client2 from "../assests/images/client2.png";
-import client3 from "../assests/images/client3.png";
-import client4 from "../assests/images/client4.png";
 
 const Agents = () => {
-    const clients = [
-        {
-          id: 1,
-          img: client1,
-          title: "Tom Wilson",
-          description: "Marketing",
-        },
-        {
-          id: 2,
-          img: client2,
-          title: "lilly potter",
-          description: " service supporter",
-        },
-        {
-          id: 3,
-          img: client3,
-          title: "Mukesh ambani",
-          description: " agency",
-        },
-        {
-          id: 4,
-          img: client4,
-          title: "john wick",
-          description: "sales",
-        },
-      ];
+  const clients = [
+    {
+      id: 1,
+      img: client1,
+      title: "Tom Wilson",
+      description: "Marketing",
+    },
+    {
+      id: 2,
+      img: client2,
+      title: "Lilly Potter",
+      description: "Service Supporter",
+    },
+  ];
+
   return (
-    <div className=" p-5 mx-10">
-    <h1 className="text-2xl md:text-4xl lg:text-5xl font-semibold font-poppins text-center mb-3 uppercase m-10">
-      Meet our agents
-    </h1>
+    <div className="flex items-center justify-center min-h-screen p-10 bg-gray-50">
+      <div className="max-w-screen-md w-full space-y-10">
+        <h1 className="text-2xl md:text-4xl lg:text-5xl font-semibold font-poppins text-center uppercase">
+          Meet our agents
+        </h1>
 
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 py-10">
-      {clients.map((client) => (
-        <div
-          key={client.id}
-          className="bg-white text-center pb-5 shadow-md rounded-lg"
-        >
-          <img
-            src={client.img}
-            alt={client.title}
-            className="w-full h-64 object-cover mb-4 rounded-md"
-          />
-          <h2 className="text-2xl font-semibold mb-2 font-playfair">
-            {client.title}
-          </h2>
-          <p className="text-sm text-gray-600 px-2 mb-4 font-playfair">
-            {client.description}
-          </p>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+          {clients.map((client) => (
+            <div
+              key={client.id}
+              className="bg-white text-center pb-5 shadow-lg rounded-lg"
+            >
+              <img
+                src={client.img}
+                alt={client.title}
+                className="w-full h-64 object-cover mb-4 rounded-t-md"
+              />
+              <h2 className="text-2xl font-semibold mb-2 font-playfair">
+                {client.title}
+              </h2>
+              <p className="text-sm text-gray-600 px-4 mb-4 font-playfair">
+                {client.description}
+              </p>
+            </div>
+          ))}
         </div>
-      ))}
+      </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default Agents
+export default Agents;
