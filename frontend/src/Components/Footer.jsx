c55dcece25aa854ea72cdc4c0917de72adc704cf
Janith-Nanayakkara <141998810+JanithNanayakkaraSE0612@users.noreply.github.com>
2024-11-02 15:11:09 +0530

    Update Footer change

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -1,4 +1,4 @@
-import  { useState } from "react";
+import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faFacebookF,
@@ -19,16 +19,21 @@ const Footer = () => {
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4 justify-center h-auto bg-[#6F97FF] p-4">
         <div className="text-left">
           <h2 className="lg:px-10 font-moul text-1xl font-bold tracking-tight text-white sm:text-3xl sm:leading-none">
-          NobleHome
+            NobleHome
             <span className="tracking-[.25em]">
               <br /> Galle
             </span>
           </h2>
           <h2 className="lg:px-10 mt-5 font-moul text-1xl font-medium tracking-tight text-white sm:text-1xl sm:leading-none">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
+            Discover exceptional living in Galle with NobleHome. Our properties
+            combine comfort, elegance, and convenience to offer you a place
+            you’ll be proud to call home. Whether you're searching for a cozy
+            residence, a spacious family home, or a prime commercial space,
+            NobleHome provides curated options tailored to your lifestyle.
+            Experience seamless buying, selling, and renting services designed
+            to make your real estate journey as effortless as possible. Find
+            your dream property with NobleHome Galle, where your ideal space
+            awaits..
           </h2>
           <div className="lg:px-10 flex space-x-4 mt-4">
             <a
@@ -125,7 +130,7 @@ const Footer = () => {
           </h2>
           <br />
           <h2 className="lg:px-10 font-moul text-1xl font-bold tracking-tight text-white sm:text-3xl sm:leading-none">
-          noblehomegalle15@gmail.com
+            noblehomegalle15@gmail.com
           </h2>
           <br />
         </div>
