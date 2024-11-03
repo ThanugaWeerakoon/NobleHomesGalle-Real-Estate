00018931be3a8a0eda4b4c86f860c6845d2b1e30
ThanugaWeerakoon <136422410+ThanugaWeerakoon@users.noreply.github.com>
2024-11-03 17:35:52 +0530

    houses

--- frontend/src/data/properties.js
+++ frontend/src/data/properties.js
@@ -1,80 +1,108 @@
-import img1 from "../assests/Land/land1.jpeg";
-import img2 from "../assests/Land/land2.jpeg";
-import img3 from "../assests/Land/land3.jpeg";
-import img4 from "../assests/Land/land4.png";
-import img5 from "../assests/Land/land5.png";
-import img6 from "../assests/Land/land6.jpeg";
+import img1 from "../assests/Land/H1.jpg";
+import img2 from "../assests/Land/L1.jpg";
+import img3 from "../assests/Land/H2.jpg";
+import img4 from "../assests/Land/L2.jpg";
+import img5 from "../assests/Land/L3.jpg";
+import img6 from "../assests/Land/L4.jpg";
+import img7 from "../assests/Land/H3.jpg";
+import img8 from "../assests/Land/H4.jpg";
+import img9 from "../assests/Land/H5.jpg";
 
 const properties = [
   {
     img: img1,
     province: "Southern Province",
-    town: "21 Acres Perches Land for Sale in Ella",
-    purches: "15 Purches",
-    price: "Rs.55,000,000",
+    town: "Bentota - Two Stories Villa",
+    purches: "0 Perches",
+    price: "LKR 68,000,000",
     id: 1,
-    city: "Gampaha", 
+    city: " Obadawatta", 
     propertyType: "House", 
   },
-  {
-    img: img1,
-    province: "Southern Province",
-    town: "21 Acres Perches Land for Sale in Ella",
-    purches: "15 Purches",
-    price: "Rs.55,000,000",
-    id: 1,
-    city: "Gampaha", 
-    propertyType: "Land", 
-  },
+
+
   {
     img: img2,
-    province: "Western Province",
-    town: "Malabe Town - Land for Sale",
-    purches: "10 Purches",
-    price: "Rs.65,000,000",
+    province: "Southern Province",
+    town: "Baddegama - Cinnamon and Tea Estate for Sale",
+    purches: "30 acres",
+    price: "LKR 9,000,000 per Acre",
     id: 2,
-    city: "Colombo", 
+    city: "Baddegama", 
     propertyType: "Land", 
   },
   {
     img: img3,
-    province: "Central Province",
-    town: "19.55 Perches Land for Sale in Madiwela",
-    purches: "12 Purches",
-    price: "Rs.45,000,000",
+    province: "Southern Province",
+    town: " House for Sale close to Talagaha junction",
+    purches: "0 Purches",
+    price: "LKR 18,000,000",
     id: 3,
-    city: "Kandy",
-    propertyType: "Land", 
+    city: "Galle",
+    propertyType: "House", 
   },
   {
     img: img4,
-    province: "Eastern Province",
-    town: "Land for Sale in Padukka Watareka",
-    purches: "8 Purches",
-    price: "Rs.35,000,000",
+    province: "Southern Province",
+    town: "Unawatuna bareland for sale close to Milton Beach",
+    purches: "100 Purches",
+    price: "LKR 650,000 per Perch",
     id: 4,
-    city: "Galle",
+    city: "Unawatuna",
     propertyType: "Land", 
   },
   {
     img: img5,
-    province: "Northern Province",
-    town: "Most valuable land for sale in Watareka",
-    purches: "20 Purches",
-    price: "Rs.75,000,000",
+    province: "Southern Province",
+    town: "Valuable Tea and Cinnamon Estate for sale in Baddegama",
+    purches: "20 Acres",
+    price: "LKR 6,000,000 per Acre",
     id: 5,
-    city: "Gampaha",
+    city: "Baddegama",
     propertyType: "Land", 
   },
   {
     img: img6,
-    province: "North Western Province",
-    town: "Most valuable land for sale in Watareka",
-    purches: "18 Purches",
-    price: "Rs.50,000,000",
+    province: "Southern Province",
+    town: "Valuable Land for Sale in  Galle at Hiribura junction close to Karapitiya hospitals",
+    purches: "47 Purches",
+    price: "LKR 1,800,000 per Perch",
     id: 6,
-    city: "Colombo",
+    city: "Galle",
     propertyType: "Land", 
+  },
+
+  {
+    img: img7,
+    province: "Southern Province",
+    town: "House for Sale at Tal-armba second blocks of the Beach Single Story House with 3 Bedrooms and 2 Bathrooms",
+    purches: "26 Purches",
+    price: "LKR 55,000,000",
+    id: 7,
+    city: "Mirissa",
+    propertyType: "House", 
+  },
+
+  {
+    img: img8,
+    province: "Southern Province",
+    town: " Midigama, 3 Bedrooms Small Villa for Sale",
+    purches: "0 Purches",
+    price: "LKR 55,000,000",
+    id: 8,
+    city: "Midigama",
+    propertyType: "House", 
+  },
+
+  {
+    img: img9,
+    province: "Southern Province",
+    town: "Single Storey 3 bedroom house and land in Wanchawala",
+    purches: "17.5 Purches",
+    price: "LKR 35,000,000",
+    id: 9,
+    city: "Galle",
+    propertyType: "House", 
   },
 ];
 
