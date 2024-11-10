300ea4ff1a63a5613490e26f41d8763ce46706b0
ThanugaWeerakoon <136422410+ThanugaWeerakoon@users.noreply.github.com>
2024-11-11 01:00:49 +0530

    updated #1

--- frontend/src/data/data.js
+++ frontend/src/data/data.js
@@ -1,109 +1,119 @@
-import img1 from "../assests/Land/H1.jpg";
-import img2 from "../assests/Land/L1.jpg";
-import img3 from "../assests/Land/H2.jpg";
-import img4 from "../assests/Land/L2.jpg";
-import img5 from "../assests/Land/L3.jpg";
-import img6 from "../assests/Land/L4.jpg";
-import img7 from "../assests/Land/H3.jpg";
-import img8 from "../assests/Land/H4.jpg";
-import img9 from "../assests/Land/H5.jpg";
+import H1 from "../assests/PropertyType/House/H1/H1.png";
+import H2 from "../assests/PropertyType/House/H2/H2.png";
+import H3 from "../assests/PropertyType/House/H3/H3.png";
+import H4 from "../assests/PropertyType/House/H4/H4.png";
+import H5 from "../assests/PropertyType/House/H5/H5.png";
+import H6 from "../assests/PropertyType/House/H6/H6.png";
+import H7 from "../assests/PropertyType/House/H7/H7.png";
+import H8 from "../assests/PropertyType/House/H8/H8.png";
+import L1 from "../assests/PropertyType/Land/L1/L1.png";
+import L2 from "../assests/PropertyType/Land/L2/L2.png";
+
 
 const properties = [
   {
-    img: img1,
+    img: H1,
     province: "Southern Province",
-    town: "Bentota - Two Stories Villa",
-    purches: "0 Perches",
-    price: "LKR 68,000,000",
+    town: "Galle - Kitulampitiya - A Modern Charming House",
+    purches: "21.5 Perches",
+    price: "LKR 90,000,000",
     id: 1,
-    city: " Obadawatta", 
+    city: " Galle", 
     propertyType: "House", 
   },
-
-
   {
-    img: img2,
+    img: H2,
     province: "Southern Province",
-    town: "Baddegama - Cinnamon and Tea Estate for Sale",
-    purches: "30 acres",
-    price: "LKR 9,000,000 per Acre",
+    town: "Galle - Kathaluwa - An Antique House Sell Close To Kabalana Beach",
+    purches: "82 Perches",
+    price: "LKR 400,000,000",
     id: 2,
-    city: "Baddegama", 
-    propertyType: "Land", 
+    city: " Kathaluwa", 
+    propertyType: "House", 
   },
   {
-    img: img3,
+    img: H3,
     province: "Southern Province",
-    town: " House for Sale close to Talagaha junction",
-    purches: "0 Purches",
-    price: "LKR 18,000,000",
+    town: "Mirissa - Kotawila - A House and Business Centre",
+    purches: "34 Perches",
+    price: "LKR 70,000,000",
     id: 3,
-    city: "Galle",
+    city: " Mirissa", 
     propertyType: "House", 
   },
   {
-    img: img4,
-    province: "Southern Province",
-    town: "Unawatuna bareland for sale close to Milton Beach",
-    purches: "100 Purches",
-    price: "LKR 650,000 per Perch",
+    img: H4,
+    province: "Western Province",
+    town: "Colombo - Homagama - 2 Stories House",
+    purches: "32.40 Perches",
+    price: "LKR 30,000,000",
     id: 4,
-    city: "Unawatuna",
-    propertyType: "Land", 
+    city: " Homagama", 
+    propertyType: "House", 
   },
   {
-    img: img5,
+    img: H5,
     province: "Southern Province",
-    town: "Valuable Tea and Cinnamon Estate for sale in Baddegama",
-    purches: "20 Acres",
-    price: "LKR 6,000,000 per Acre",
+    town: "Ahangama - 2BR - Repairable House",
+    purches: "52 Perches",
+    price: "LKR 21,000,000",
     id: 5,
-    city: "Baddegama",
-    propertyType: "Land", 
+    city: " Ahangama", 
+    propertyType: "House", 
   },
   {
-    img: img6,
+    img: H6,
     province: "Southern Province",
-    town: "Valuable Land for Sale in  Galle at Hiribura junction close to Karapitiya hospitals",
-    purches: "47 Purches",
-    price: "LKR 1,800,000 per Perch",
+    town: "Galle - A Colonial House",
+    purches: "21 Perches",
+    price: "LKR 80,000,000",
     id: 6,
-    city: "Galle",
+    city: " Galle", 
+    propertyType: "House", 
+  },
+  {
+    img: H7,
+    province: "Southern Province",
+    town: "Galle - Kurunduwatta - 2 Stories House And Shop Space",
+    purches: "43 Perches",
+    price: "LKR 65,000,000",
+    id: 7,
+    city: " Kurunduwatta", 
+    propertyType: "House", 
+  },
+  {
+    img: H8,
+    province: "Southern Province",
+    town: "Matara - Paramulla- Two Stories Box House",
+    purches: "17 Perches",
+    price: "LKR 35,000,000",
+    id: 8,
+    city: " Paramulla", 
+    propertyType: "House", 
+  },
+  {
+    img: L1,
+    province: "Southern Province",
+    town: "Galle-Hiribura Junction- A Residential/Commercial Land (Rs. 2mn per perch) Negotiable",
+    purches: "47 Perches",
+    price: "LKR 2,000,000",
+    id: 9,
+    city: " Galle", 
+    propertyType: "Land", 
+  },
+  {
+    img: L2,
+    province: "Southern Province",
+    town: "Ella - Ambadandegama -17 Acres Bareland ",
+    purches: "17 Acres",
+    price: "LKR 40,000,000 ",
+    id: 10,
+    city: " Ella", 
     propertyType: "Land", 
   },
 
-  {
-    img: img7,
-    province: "Southern Province",
-    town: "House for Sale at Tal-armba second blocks of the Beach Single Story House with 3 Bedrooms and 2 Bathrooms",
-    purches: "26 Purches",
-    price: "LKR 55,000,000",
-    id: 7,
-    city: "Mirissa",
-    propertyType: "House", 
-  },
 
-  {
-    img: img8,
-    province: "Southern Province",
-    town: " Midigama, 3 Bedrooms Small Villa for Sale",
-    purches: "0 Purches",
-    price: "LKR 55,000,000",
-    id: 8,
-    city: "Galle",
-    propertyType: "House", 
-  },
 
-  {
-    img: img9,
-    province: "Southern Province",
-    town: "Single Storey 3 bedroom house and land in Wanchawala",
-    purches: "17.5 Purches",
-    price: "LKR 35,000,000",
-    id: 9,
-    city: "Galle",
-    propertyType: "House", 
-  },
 ];
 
 export default properties;
--- frontend/src/properties.js
+++ frontend/src/properties.js
@@ -1,52 +1,187 @@
 
-import h11 from "././assests/Data/H1/h11.jpg";
-import h12 from "././assests/Data/H1/h12.jpg";
-import h13 from "././assests/Data/H1/h13.jpg";
-import h14 from "././assests/Data/H1/h14.jpg";
-import h15 from "././assests/Data/H1/h15.jpg";
+import H1 from "././assests/PropertyType/House/H1/H1.1.png";
+import H102 from "././assests/PropertyType/House/H1/H1.2.png";
+import H103 from "././assests/PropertyType/House/H1/H1.3.png";
+import H104 from "././assests/PropertyType/House/H1/H1.4.png";
+import H105 from "././assests/PropertyType/House/H1/H1.5.png";
+import H106 from "././assests/PropertyType/House/H1/H1.6.png";
 
+import H2 from "././assests/PropertyType/House/H2/H2.png";
+import H201 from "././assests/PropertyType/House/H2/H2.1.png";
+import H202 from "././assests/PropertyType/House/H2/H2.2.png";
+import H203 from "././assests/PropertyType/House/H2/H2.4.png";
+import H204 from "././assests/PropertyType/House/H2/H2.5.png";
+import H205 from "././assests/PropertyType/House/H2/H2.6.png";
+import H206 from "././assests/PropertyType/House/H2/H2.7.png";
+import H207 from "././assests/PropertyType/House/H2/H2.8.png";
+
+import H3 from "././assests/PropertyType/House/H3/H3.png";
+import H301 from "././assests/PropertyType/House/H3/H3.1.png";
+import H302 from "././assests/PropertyType/House/H3/H3.2.png";
+import H303 from "././assests/PropertyType/House/H3/H3.3.png";
+import H304 from "././assests/PropertyType/House/H3/H3.4.png";
+import H305 from "././assests/PropertyType/House/H3/H3.5.png";
+import H306 from "././assests/PropertyType/House/H3/H3.6.png";
+import H307 from "././assests/PropertyType/House/H3/H3.7.png";
+
+import H4 from "././assests/PropertyType/House/H4/H4.png";
+import H401 from "././assests/PropertyType/House/H4/H4.1.png";
+import H402 from "././assests/PropertyType/House/H4/H4.2.png";
+import H403 from "././assests/PropertyType/House/H4/H4.3.png";
+import H404 from "././assests/PropertyType/House/H4/H4.4.png";
+import H405 from "././assests/PropertyType/House/H4/H4.5.png";
+import H406 from "././assests/PropertyType/House/H4/H4.6.png";
+import H407 from "././assests/PropertyType/House/H4/H4.7.png";
+
+import H5 from "././assests/PropertyType/House/H5/H5.png";
+import H501 from "././assests/PropertyType/House/H5/H5.1.png";
+
+import H6 from "././assests/PropertyType/House/H6/H6.png";
+import H601 from "././assests/PropertyType/House/H6/H6.1.png";
+import H602 from "././assests/PropertyType/House/H6/H6.2.png";
+import H603 from "././assests/PropertyType/House/H6/H6.3.png";
+
+import H7 from "././assests/PropertyType/House/H7/H7.png";
+import H701 from "././assests/PropertyType/House/H7/H7.1.png";
+import H702 from "././assests/PropertyType/House/H7/H7.2.png";
+
+import H8 from "././assests/PropertyType/House/H8/H8.png";
+import H801 from "././assests/PropertyType/House/H8/H8.1.png";
+import H802 from "././assests/PropertyType/House/H8/H8.2.png";
+import H803 from "././assests/PropertyType/House/H8/H8.3.png";
+import H804 from "././assests/PropertyType/House/H8/H8.4.png";
+import H805 from "././assests/PropertyType/House/H8/H8.5.png";
+import H806 from "././assests/PropertyType/House/H8/H8.6.png";
+import H807 from "././assests/PropertyType/House/H8/H8.7.png";
+
+import L1 from "././assests/PropertyType/Land/L1/L1.png";
+import L101 from "././assests/PropertyType/Land/L1/L1.1.png";
+import L102 from "././assests/PropertyType/Land/L1/L1.2.png";
+import L103 from "././assests/PropertyType/Land/L1/L1.3.png";
+
+import L2 from "././assests/PropertyType/Land/L2/L2.png";
+import L201 from "././assests/PropertyType/Land/L2/L2.1.png";
+import L202 from "././assests/PropertyType/Land/L2/L2.2.png";
+import L203 from "././assests/PropertyType/Land/L2/L2.3.png";
+import L204 from "././assests/PropertyType/Land/L2/L2.4.png";
+import L205 from "././assests/PropertyType/Land/L2/L2.5.png";
+import L206 from "././assests/PropertyType/Land/L2/L2.6.png";
+import L207 from "././assests/PropertyType/Land/L2/L2.7.png";
 
 
 const propertyDetails = [
   {
     id: 1,
-    description: "A beautiful two-story villa located in Bentota, offering modern living with spacious rooms and a large backyard.",
-    contactInfo: "Contact: 077-XXXXXXX",
-    agentName: "John Doe",
+    description: "These modern and charming semi two stories house and 21.5 Perches land extent are situated in Galle at Kitulampitiya. The main house has 3 bedrooms and two bathrooms. One of it's a master bedroom with a attached bathroom, air conditioning and heating system and bathtub. Adjoining the master bedroom has a wardrobe and dressing room and ironing place. Two other rooms has one shared bathroom and a office room. Two pantries with all made in Germany gas cooker and oven. And a separate wet kitchen. And two bedrooms apartment with a pantry, kitchen and living area and bathroom. As well as three spacious balconies. Seperate entrance outside and inside the main house. The electricity supply is solar electricity and no electricity bill. Solar hot water system, 5000 liters water tanks with pressure pump. Well maintained garden and highly built parapet wall and two electronic gates. Fully house floors are Rocell tiled and timber ceilings. The entire ground floor has fully air-conditioned systems with 7 air conditioning systems.( As electricity supply is solar electricity no electricity bills) To Karapitiya hospitals district is nearly 2 km. To Galle main city is 5 km. To Pinnaduwa highway entrance is 8 km. All amenities available a few walking distance away from the property. Calm and quite place. Very close to the government and private English schools. Hassles free access to the property and all amenities such as supermarkets, filling stations, temples and shrines, schools and hospitals. And internet and digital Access. For more information and personal showing please contact our real estate agent.",
+    contactInfo: "Contact: 070-1688980",
+    agentName: "Tilak",
     agentPhone: "077-XXXXXXX",
-    features: ["4 Bedrooms", "3 Bathrooms", "Large Garden", "Garage"],
+    features: ["4 Bedrooms", "3 Bathrooms", "3 Car parking spaces", " 2 No. of floors", " AC Rooms", " 24 Hour Security", " Mainline Water", " Luxury Specs", " 3 Phase Electricity", " Home Security System", " Roof Top Garden", " Garage", " Maid's Room", " Maid's Toilet"],
     images: [
-      h11,h12,h13,h14,h15,
+      H1,H102,H103,H104,H105,H106
       
     ],
   },
   {
     id: 2,
-    description: "A prime piece of cinnamon and tea estate for sale located in Baddegama, with breathtaking views and fertile soil.",
-    contactInfo: "Contact: 078-XXXXXXX",
-    agentName: "Jane Doe",
-    agentPhone: "078-XXXXXXX",
-    features: ["30 Acres of Land", "Cinnamon Trees", "Tea Plantations"],
+    description: "These almost 200 years old house and 82 perches land are situated in Galle at Kabalana well popular among foreigners, 110 meters away from the Kabalana beach. The house can be used after minor repairs and refurbish. No furnished. More spaces for any new development. The Kabalana beach views to the property gate. The garden has coconut and some fruit trees. The Kabalana beach is well popular among foreigners for surfing, under water diving and bathing and walking. Its ideal investment investors who look to buy a property at Kabalana. For more information and personal showing please contact our real estate agent.",
+    contactInfo: "Contact: 070-1688980",
+    agentName: "Tilak",
+    agentPhone: "077-XXXXXXX",
+    features: ["6 Bedrooms", "2 Bathrooms", "10 Car parking spaces", " 2 No. of floors", "Unfurnished", " Beachfront / Sea View", " Mainline Water", " Colonial Architecture", " Lawn Garden", " Attached Toilets", " Overhead Water Storage"],
     images: [
-      "https://example.com/img4.jpg",
-      "https://example.com/img5.jpg",
-      "https://example.com/img6.jpg",
+      H2,H201,H202,H203,H204,H205,H206,H207
     ],
   },
   {
     id: 3,
-    description: "A house for sale located close to Talagaha junction in Galle, ideal for a family looking for a spacious property.",
-    contactInfo: "Contact: 079-XXXXXXX",
-    agentName: "Alice Smith",
-    agentPhone: "079-XXXXXXX",
-    features: ["3 Bedrooms", "2 Bathrooms", "Spacious Living Room"],
+    description: "There are two stories house and on going business premises located in mirissa, Kotawila. (Labeema new town road facing) 3 bedrooms, a pantry, a spacious living area on the ground floor and two bedrooms with a shared bathroom on the second floors. The ground floor has tiled floors and ceiling. The second floor has red cemented. The business premises adjoining to the main house and its on going households items. For more information and personal showing please contact our real estate agent.",
+    contactInfo: "Contact: 070-1688980",
+    agentName: "Tilak",
+    agentPhone: "077-XXXXXXX",
+    features: ["5 Bedrooms", "2 Bathrooms", "2 Car parking spaces", " 2 No. of floors", "Unfurnished", " AC Rooms", " 24 Hour Security", " Luxury Specs", " Hot Water", " Attached Toilets", " Mainline Water", " Maid's Toilet"],
     images: [
-      "https://example.com/img7.jpg",
-      "https://example.com/img8.jpg",
-      "https://example.com/img9.jpg",
+      H3,H301,H302,H303,H304,H305,H306,H307
     ],
   },
-  // Add more property details as needed...
+  {
+    id: 4,
+    description: "There are two stories house and 32.40 perches land extent located in Colombo in Homagama. Address - Pitipana South, Homagama, Land size - 32.40 perches, No of bed rooms -4 (master bed room Ac), Floor type - tiles (ground floor and first floor) ,Two kitchens wet kitchen and dry kitchen, Wooden Ceiling, House squar feet- 3000, Age of the house- 16, Electricity type - single phase, 20 ft Width road access, Parking - available, Nearest town homagama 4 km, 1.8 km to NSBM, Keels Supermarket, Mahinda Rajapakasha Collage Pitipana Homagama, Facility of Technology University of Colombo 1.8km, Sri Lanka institute of Nano Technology, Telecom data center, A part of Moratuwa university. ,It's an ideal investment opportunity for investors who looking to buy a two stories house close to the Homagama and Colombo, For more information and personal showing please contact our real estate agent.",
+    contactInfo: "Contact: 070-1688980",
+    agentName: "Tilak",
+    agentPhone: "077-XXXXXXX",
+    features: ["2 Bedrooms", "4 Bathrooms", "2 Car parking spaces", " 2 No. of floors", "Unfurnished", " AC Rooms", " Luxury Specs", " Garage", " Attached Toilets", " Mainline Water", " Home Security System", " Home Security System"],
+    images: [
+      H4,H401,H402,H403,H404,H405,H406,H407
+    ],
+  },
+  {
+    id: 5,
+    description: "52 perches with the house, 2 bedrooms, 2.2 km to beach",
+    contactInfo: "Contact: 070-1688980",
+    agentName: "Tilak",
+    agentPhone: "077-XXXXXXX",
+    features: ["2 Bedrooms", "1 Bathrooms", "3 Car parking spaces", " 1 No. of floors", "Unfurnished"],
+    images: [
+      H5,H501
+    ],
+  },
+  {
+    id: 6,
+    description: "There are 21 perches land extent and almost 100 years old colonial type house located in Galle city boundaries at Makuluwa. There are varenda, a living area, 3 bedrooms, kitchen and dining area, courtyard, a modern bathroom and one vehicle park closed garage. City water supply and a well water. All amenities are available a few kilometers away from the property. The house structure and everything is still unchanged. Good neighborhood. Direct access to the property. The house can be use after repair and refurbishment.Sinnakkara oppu.",
+    contactInfo: "Contact: 070-1688980",
+    agentName: "Tilak",
+    agentPhone: "077-XXXXXXX",
+    features: ["4 Bedrooms", "1 Bathrooms", "2 Car parking spaces", " 1 No. of floors", "Unfurnished" , "Mainline Water" , "Colonial Architecture" , "Garage", "Lawn Garden", "Attached Toilets", "Maid's Toilet"],
+    images: [
+      H6,H601,H602,H603
+    ],
+  },
+  {
+    id: 7,
+    description: "There are 43 perches land extent and two stories four bedrooms house and drop space for sale in Galle, Udugama road at Kurunduwatta junction direct face to the Udugama main road. The property owner is planning to relocate and its going to sale immediately. It's very close to the Karapitiya hospitals district and Pinnaduwa Highway Interchange. Sinnakkara oppu. For more information and personal showing please contact our real estate agent.",
+    contactInfo: "Contact: 070-1688980",
+    agentName: "Tilak",
+    agentPhone: "077-XXXXXXX",
+    features: ["4 Bedrooms", "1 Bathrooms", "10 Car parking spaces", " 2 No. of floors", "Unfurnished" , "Luxury Specs" , "Mainline Water" , "Home Security System", "Lawn Garden", "Attached Toilets", "Maid's Room", "Maid's Toilet"],
+    images: [
+      H7,H701,H702
+    ],
+  },
+  {
+    id: 8,
+    description: "There are 4 bedrooms box type new two stories house located in Matara, Paramulla, 200 meters away from the beach. The house third floor is a rooftop and can be build another floor. Two stairs to the upper floors in and outside. Adjoining the new building has a old house with 3 more bedrooms. A spacious compound for further developments such as a swimming pool. It's really ideal investment opportunity for tourism industry. The Third floors of the building beach views. Sinnakkara oppu and its already mortgaged. For more information and personal showing please contact our real estate agent. Its going to sale as owners children's higher education.",
+    contactInfo: "Contact: 070-1688980",
+    agentName: "Tilak",
+    agentPhone: "077-XXXXXXX",
+    features: ["4 Bedrooms", "2 Bathrooms", "3 Car parking spaces", " 2 No. of floors", "Unfurnished" ,"Beachfront / Sea View" , "Luxury Specs" , "Mainline Water" , "Home Security System", "Colonial Architecture" , "Lawn Garden", "Attached Toilets", "Maid's Toilet"],
+    images: [
+      H8,H801,H802,H803,H804,H805,H806,H807
+    ],
+  },
+  {
+    id: 9,
+    description: "There are 47perches plots of residential or commercial land, located in Galle, situated at Hiribura junction, close to the Karapitiya hospitals district. Its just behind the Hiribura junction-Baddegama main road. The plot views to the main road. To Karapitiya hospitals district is 1.1 km. The Hiribura junction is next to the Karapitiya new town and very rapidly commercialisation and urbanisation. All amenities such as government and private 4 hospitals, supermarkets, filling stations, leading government and English schools, pharmacies and channeling centres, temples and shrines centres, proximity to Galle main city, and Southern highway exchange at pinnaduwa, Digital and infrastructure s, carpet road, etc. It's mostly ideal for a commercial purposes such as a apartments, private nursing home, channeling centres, a showroom, a restaurant, and any kind of commercial purpose. All documentation related to the property approved by the local authorities, electricity was already connected and city water supply was expected. (Applied by the owner) Sinnakkara oppu.",
+    contactInfo: "Contact: 070-1688980",
+    agentName: "Tilak",
+    agentPhone: "077-XXXXXXX",
+    features: ["3 Phase Electricity", "Ideal For Commercial Use", "Approved Survey Plan", " Pipe Borne Water"],
+    images: [
+      L1,L101,L102,L103
+    ],
+  },
+  {
+    id: 10,
+    description: "These 17 acres bare land are situated in Uva at Ella, close to the Ambadandegama school. Its already neglected green fields. Its ideal for a eco tourism and cultivation. There are four more road access to the property as follows. 1. Bandarawela- Ambadandegama, 2. Bandarawela- Heel Oya via Keenigama, 3. Ella bimge - Makulwella via Ambadandegama And 4. Wellawaya- Ella- Karandagolla Junction. (Its short cut and carpet road) Water and electricity available nearby the land. No house or bungalow. But a poor farmer lives the boundary of the property since a long-term. Peak of the property views Ravana Ella and Ella Rock. (far views) And there are many tourists villas and some of them are Morning Mist, Roseland Tea Factory, Laseren Eco etc. Property documents are cleared and anytime can be obtain bank loans.",
+    contactInfo: "Contact: 070-1688980",
+    agentName: "Tilak",
+    agentPhone: "077-XXXXXXX",
+    features: ["3 Phase Electricity", "Ideal For Commercial Use", "HILLY LANDSCAPE", " Pipe Borne Water"],
+    images: [
+      L2,L201,L202,L203,L204,L205,L206,L207
+    ],
+  },
+  
 ];
 
 export default propertyDetails;
